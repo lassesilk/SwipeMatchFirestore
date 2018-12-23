dbf3a6cb005966ec7af56a87246839bf2dac5858
Lasse N Silkoset <lassesilkoset@gmail.com>
2018-12-23 09:09:48 +0100

    Initial Commit

---
+++ SwipeMatchFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeMatchFirestore
+//
+//  Created by Lasse Silkoset on 23/12/2018.
+//  Copyright © 2018 Lasse Silkoset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
